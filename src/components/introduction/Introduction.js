c55595855f1765fa9f348563934c573682b50b04
nic <nic@nicoles-Mac-mini.local>
2020-02-04 15:39:52 -0500

    clean up

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -21,7 +21,7 @@ var TxtType = function(el, toRotate, period) {
         this.txt = fullTxt.substring(0, this.txt.length + 1);
         }
 
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+        this.el.innerHTML = '<span class="lickthewrapper">'+this.txt+'</span>';
 
         var that = this;
         var delta = 200 - Math.random() * 100;
@@ -63,18 +63,13 @@ var TxtType = function(el, toRotate, period) {
 const Introduction = () =>
   <section id="intro" className="wrapper style1 fullscreen fade-up">
     <div className="inner">
-
-
-<h1>
-  <a href="" class="typewrite" data-period="2000" data-type='[ "Hi, Im Si.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
-    <span class="lickthewrapper"></span>
-  </a>
-</h1>
-
-
+      <h1>
+        <a href="" class="typewrite" data-period="2000" data-type='[ "Hi, Im Si.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
+          <span class="lickthewrapper"></span>
+            </a>
+      </h1>
       <h1>Marketing strategy, brand, is a scam.</h1>
       <h2>but it doesn't have to be.  </h2>
-      </p>
       <ul className="actions">
         <li>
           <Scroll type="id" element="one">
