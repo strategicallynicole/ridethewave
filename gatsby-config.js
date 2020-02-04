81c10abb02af1ac664566aeb3566123ab8d90f7d
nic <nic@nicoles-Mac-mini.local>
2020-02-04 14:56:41 -0500

    f

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  pathPrefix: `/gatsby-starter-hyperspace/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: ``, // This path is subpath of your hosting https://domain/portfolio
   siteMetadata: {
-    title: 'Gatsby Starter Hyperspace',
+    title: 'OAKwave',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
