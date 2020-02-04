1f2539fe2e55475d579ccf39fe904fe5d5cc05be
nic <nic@nicoles-Mac-mini.local>
2020-02-04 17:05:57 -0500

    intro

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -5,11 +5,6 @@ import Scroll from '../common/Scroll';
 const Introduction = () =>
   <section id="intro" className="wrapper style1 fullscreen fade-up">
     <div className="inner">
-      <h1>
-        <a href="" class="typewrite" data-period="2000" data-type='[ "Hi, Im Si.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
-          <span class="lickthewrapper"></span>
-            </a>
-      </h1>
       <h1>Marketing strategy, brand, is a scam.</h1>
       <h2>but it doesn't have to be.  </h2>
       <ul className="actions">
